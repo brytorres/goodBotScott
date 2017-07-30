1576732330cf857b10b38ee46f7322449db05aff
brytorres <dev@bryantorres.pro>
2017-07-30 06:16:45 -0400

    Fetch from Liqui.io API and generate global data object

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,45 @@
-document.getElementById("bitcoin").addEventListener("click", function() {getPrices('bitcoin');});
-document.getElementById("ethereum").addEventListener("click", function() {getPrices('ethereum');});
-document.getElementById("litecoin").addEventListener("click", function() {getPrices('litecoin');});
+globals = {};
 
+// Listen for button clicks
+document.getElementById("bitcoin").addEventListener("click", function() {runAll('bitcoin');});
+document.getElementById("ethereum").addEventListener("click", function() {runAll('ethereum');});
+document.getElementById("litecoin").addEventListener("click", function() {runAll('litecoin');});
+
+
+// FUNCTION : fetches the current data from whichever coin string is passed in.
 function getPrices(coin){
     axios.get('https://api.coinmarketcap.com/v1/ticker/'+coin+'/',)
     .then(function(response){
     // Log response
     // console.log(response);
+    var percentageChanges = [];
 
     // Get coin components
     var name = response.data[0].name;
     var symbol = response.data[0].symbol;
     var currentPrice = parseFloat(response.data[0].price_usd).toFixed(2);
     var percentageChange1h = parseFloat(response.data[0].percent_change_1h);
+    percentageChanges.push(percentageChange1h);
+    globals['change1h'] = percentageChange1h;
     var percentageChange24h = parseFloat(response.data[0].percent_change_24h);
+    percentageChanges.push(percentageChange24h);
+    globals['change24h'] = percentageChange24h;
     var percentageChange7d = parseFloat(response.data[0].percent_change_7d);
-    var percentageChanges = [percentageChange1h, percentageChange24h, percentageChange7d]
+    percentageChanges.push(percentageChange7d);
+    globals['change7d'] = percentageChange7d;
+    // var percentageChanges = [percentageChange1h, percentageChange24h, percentageChange7d]
 
     // Get percentage change average
     function getAverage(){
         var percentagesSum = percentageChange1h + percentageChange24h + percentageChange7d;
         return percentagesSum / (percentageChanges.length);
     }
-    var percentageAverage = getAverage().toFixed(2);
+    var percentageAverage = parseFloat(getAverage().toFixed(2));
+    percentageChanges.push(percentageAverage);
+    globals['changeAverage'] = percentageAverage;
+    // console.log(percentageChanges);
 
+    // Generate current market data output
     var coinComponentsOutput = `
         <h3 class="center-align">${name}</h3>
         ${symbol}
@@ -38,22 +54,22 @@ function getPrices(coin){
             </div>
             <div class="col l3">
                 <p class="percentage-time">1 Hour:</p><br>
-                <p class="percentage-data">${percentageChange1h}</p>
+                <p class="percentage-data">${percentageChange1h} %</p>
             </div>
 
             <div class="col l3">
                 <p class="percentage-time">24 hour:</p><br>
-                <p class="percentage-data">${percentageChange24h}</p>
+                <p class="percentage-data">${percentageChange24h} %</p>
             </div>
 
             <div class="col l3">
                 <p class="percentage-time">7 day:</p><br>
-                <p class="percentage-data">${percentageChange7d}</p>
+                <p class="percentage-data">${percentageChange7d} %</p>
             </div>
 
             <div class="col l3">
                 <p class="percentage-time">Average:</p><br>
-                <p class="percentage-data">${percentageAverage}</p>
+                <p class="percentage-data">${percentageAverage} %</p>
             </div>
         </div>
     `;
@@ -65,4 +81,67 @@ function getPrices(coin){
     .catch(function(error){
         console.log(error);
     })
+}
+
+function currentTicker(coin){
+    if(coin === 'bitcoin'){
+        coinShort = 'btc';
+        var pair = coinShort+'_usdt'
+        axios.get('https://api.liqui.io/api/3/ticker/'+pair+'/')
+        .then(function(response){
+            var priceHigh = response.data.btc_usdt.high.toFixed(2);
+            globals['high'] = parseFloat(priceHigh);
+            var priceLow = response.data.btc_usdt.low.toFixed(2);
+            globals['low'] = parseFloat(priceLow);
+            var priceBuy = response.data.btc_usdt.buy.toFixed(2);
+            globals['by'] = parseFloat(priceBuy);
+            var priceSell = response.data.btc_usdt.sell.toFixed(2);
+            globals['sell'] = parseFloat(priceSell);
+            var priceLast = response.data.btc_usdt.last.toFixed(2);
+            globals['last'] = parseFloat(priceLast);
+            console.log(globals);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    } else if(coin === 'ethereum'){
+        coinShort = 'eth';
+        var pair = coinShort+'_usdt'
+        axios.get('https://api.liqui.io/api/3/ticker/'+pair+'/')
+        .then(function(response){
+            var priceHigh = response.data.eth_usdt.high.toFixed(2);
+            globals['high'] = parseFloat(priceHigh);
+            var priceLow = response.data.eth_usdt.low.toFixed(2);
+            globals['low'] = parseFloat(priceLow);
+            var priceBuy = response.data.eth_usdt.buy.toFixed(2);
+            globals['buy'] = parseFloat(priceBuy);
+            var priceSell = response.data.eth_usdt.sell.toFixed(2);
+            globals['sell'] = parseFloat(priceSell);
+            var priceLast = response.data.eth_usdt.last.toFixed(2);
+            globals['last'] = parseFloat(priceLast);
+            console.log(globals);
+        })
+    } else {
+        coinShort = 'ltc';
+        var pair = coinShort+'_usdt'
+        axios.get('https://api.liqui.io/api/3/ticker/'+pair+'/')
+        .then(function(response){
+            var priceHigh = response.data.ltc_usdt.high.toFixed(2);
+            globals['high'] = parseFloat(priceHigh);
+            var priceLow = response.data.ltc_usdt.low.toFixed(2);
+            globals['low'] = parseFloat(priceLow);
+            var priceBuy = response.data.ltc_usdt.buy.toFixed(2);
+            globals['by'] = parseFloat(priceBuy);
+            var priceSell = response.data.ltc_usdt.sell.toFixed(2);
+            globals['sell'] = parseFloat(priceSell);
+            var priceLast = response.data.ltc_usdt.last.toFixed(2);
+            globals['last'] = parseFloat(priceLast);
+            console.log(globals);
+        })
+    }
+}
+
+function runAll(coin){
+    getPrices(coin);
+    currentTicker(coin);
 }
