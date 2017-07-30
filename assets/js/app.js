38b837be8ee4e92a79b11284db0259492249da42
brytorres <dev@bryantorres.pro>
2017-07-30 14:20:25 -0400

    Fix border colors and spread math

--- assets/js/app.js
+++ assets/js/app.js
@@ -43,12 +43,12 @@ function getPrices(coin){
     var coinComponentsOutput = `
         <h3 class="center-align">${name}</h3>
         ${symbol}
-        <div class="box price">
+        <div class="box price valign-wrapper">
             <h3 class="box">Current Price:</h3> 
             <h4 class="box">$${currentPrice}</h4>
         </div>
         <br>
-        <div class="box price">
+        <div class="box percentage">
             <div class="col l12">
                 <h5 class="center">Percentage Change</h5>
             </div>
@@ -88,6 +88,8 @@ function currentTicker(coin){
     if(coin === 'bitcoin'){
         coinShort = 'btc';
         var pair = coinShort+'_usdt'
+
+        // Get BTC Data
         axios.get('https://api.liqui.io/api/3/ticker/'+pair+'/')
         .then(function(response){
             var priceHigh = response.data.btc_usdt.high.toFixed(2);
@@ -100,8 +102,9 @@ function currentTicker(coin){
             globals['sell'] = parseFloat(priceSell);
             var priceLast = response.data.btc_usdt.last.toFixed(2);
             globals['last'] = parseFloat(priceLast);
-            var spread = parseFloat(priceLow - priceHigh).toFixed(2);
+            var spread = parseFloat(priceHigh - priceLow).toFixed(2);
 
+            // Generate BTC HTML Ouput
             var tickerOutput = `
             <div class="box price">
                 <div class="col l12">
@@ -119,7 +122,6 @@ function currentTicker(coin){
                     <p class="percentage-time">Spread:</p>
                     <p class="percentage-data" style="padding-top: 10px">$${spread}</p>
                 </div>
-                <hr>
                 <div class="col l4">
                     <p class="percentage-time">Buy:</p>
                     <p class="percentage-data" style="padding-top: 10px">$${priceBuy}</p>
@@ -144,6 +146,8 @@ function currentTicker(coin){
     } else if(coin === 'ethereum'){
         coinShort = 'eth';
         var pair = coinShort+'_usdt'
+
+        // Get ETH Data
         axios.get('https://api.liqui.io/api/3/ticker/'+pair+'/')
         .then(function(response){
             var priceHigh = response.data.eth_usdt.high.toFixed(2);
@@ -156,10 +160,10 @@ function currentTicker(coin){
             globals['sell'] = parseFloat(priceSell);
             var priceLast = response.data.eth_usdt.last.toFixed(2);
             globals['last'] = parseFloat(priceLast);
-            var spread = parseFloat(priceLow - priceHigh).toFixed(2);
+            var spread = parseFloat(priceHigh - priceLow).toFixed(2);
 
+            // Generate ETH HTML Output
             var tickerOutput = `
-            <br>
             <div class="box price">
                 <div class="col l12">
                     <h5 class="center">Market Ticker</h5>
@@ -176,7 +180,6 @@ function currentTicker(coin){
                     <p class="percentage-time">Spread:</p>
                     <p class="percentage-data" style="padding-top: 10px">$${spread}</p>
                 </div>
-                <hr>
                 <div class="col l4">
                     <p class="percentage-time">Buy:</p>
                     <p class="percentage-data" style="padding-top: 10px">$${priceBuy}</p>
@@ -198,6 +201,8 @@ function currentTicker(coin){
     } else {
         coinShort = 'ltc';
         var pair = coinShort+'_usdt'
+
+        // Get LTC Data
         axios.get('https://api.liqui.io/api/3/ticker/'+pair+'/')
         .then(function(response){
             var priceHigh = response.data.ltc_usdt.high.toFixed(2);
@@ -210,10 +215,10 @@ function currentTicker(coin){
             globals['sell'] = parseFloat(priceSell);
             var priceLast = response.data.ltc_usdt.last.toFixed(2);
             globals['last'] = parseFloat(priceLast);
-            var spread = parseFloat(priceLow - priceHigh).toFixed(2);
+            var spread = parseFloat(priceHigh - priceLow).toFixed(2);
 
+            // Generate LTC HTML Output
             var tickerOutput = `
-            <br>
             <div class="box price">
                 <div class="col l12">
                     <h5 class="center">Market Ticker</h5>
@@ -230,7 +235,6 @@ function currentTicker(coin){
                     <p class="percentage-time">Spread:</p>
                     <p class="percentage-data" style="padding-top: 10px">$${spread}</p>
                 </div>
-                <hr>
                 <div class="col l4">
                     <p class="percentage-time">Buy:</p>
                     <p class="percentage-data" style="padding-top: 10px">$${priceBuy}</p>
